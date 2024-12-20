e964d00c81c3150ac7af059356580d46c0ce85a2
Emma <emma.malinsdotter@outlook.com>
2024-12-20 15:02:41 +0100

    la ut lite kod i kommentarer som inte ännu funkar

--- JavaScript-mapp/kvitto.js
+++ JavaScript-mapp/kvitto.js
@@ -1,42 +1,42 @@
-import { getReceipt } from "./hämtaAPI.js";
+// import { getReceipt } from "./hämtaAPI.js";
 
 
 
 
-document.querySelector(".sekvittot").addEventListener("click", async () => {
-	if (orderData) {
-		try {
-			const receiptArray = await getReceipt({ order: { id: orderData.id } });
-			if (receiptArray && receiptArray.receipt.items) {
-				const receiptContainer = document.querySelector(".valmatkvitto");
-				receiptContainer.innerHTML = "";  
+// document.querySelector(".sekvittot").addEventListener("click", async () => {
+// 	if (orderData) {
+// 		try {
+// 			const receiptArray = await getReceipt({ order: { id: orderData.id } });
+// 			if (receiptArray && receiptArray.receipt.items) {
+// 				const receiptContainer = document.querySelector(".valmatkvitto");
+// 				receiptContainer.innerHTML = "";  
 
-				receiptArray.receipt.items.forEach((item) => {
-					const receiptItemName = document.createElement("p");
-					receiptItemName.classList.add("matval");
-					receiptItemName.innerText = item.name;
+// 				receiptArray.receipt.items.forEach((item) => {
+// 					const receiptItemName = document.createElement("p");
+// 					receiptItemName.classList.add("matval");
+// 					receiptItemName.innerText = item.name;
 
-					const receiptDivider = document.createElement("div");
-					receiptDivider.classList.add("mellanstreckkvitto");
+// 					const receiptDivider = document.createElement("div");
+// 					receiptDivider.classList.add("mellanstreckkvitto");
 
-					const receiptItemPrice = document.createElement("p");
-					receiptItemPrice.classList.add("matpris");
-					receiptItemPrice.innerText = `${item.price} SEK`;
+// 					const receiptItemPrice = document.createElement("p");
+// 					receiptItemPrice.classList.add("matpris");
+// 					receiptItemPrice.innerText = `${item.price} SEK`;
 
-					receiptContainer.appendChild(receiptItemName);
-					receiptContainer.appendChild(receiptDivider);
-					receiptContainer.appendChild(receiptItemPrice);
-				});
+// 					receiptContainer.appendChild(receiptItemName);
+// 					receiptContainer.appendChild(receiptDivider);
+// 					receiptContainer.appendChild(receiptItemPrice);
+// 				});
 
-				// Totalbelopp
-				const totalElement = document.querySelector(".slutpris p");
-				totalElement.innerText = `${receiptArray.receipt.total} SEK`;
-			}
-		} catch (error) {
-			console.error("Error fetching receipt:", error);
-		}
-	} else {
-		console.error("Orderdata saknas. Kan inte hämta kvitto.");
-	}
-});
+// 				// Totalbelopp
+// 				const totalElement = document.querySelector(".slutpris p");
+// 				totalElement.innerText = `${receiptArray.receipt.total} SEK`;
+// 			}
+// 		} catch (error) {
+// 			console.error("Error fetching receipt:", error);
+// 		}
+// 	} else {
+// 		console.error("Orderdata saknas. Kan inte hämta kvitto.");
+// 	}
+// });
 
